c8ef307d764e18bc9dd05d0385a1c6ce0eeb83a8
farrem68 <matthew.farrelly68@mail.dcu.ie>
2020-04-19 13:21:22 +0100

    changes

--- test/src/personal_portfolio/urls.py
+++ test/src/personal_portfolio/urls.py
@@ -11,7 +11,6 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -25,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls"), name='projects'),
     path("register", user_views.register, name ='register'),
-    # path("createListing/", include("projects.urls"), name ='createListing'),
     path("profile/", user_views.profile, name ='profile'),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name ='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name ='logout'),
--- test/src/users/forms.py
+++ test/src/users/forms.py
@@ -8,7 +8,6 @@ from django.forms import ModelForm
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
